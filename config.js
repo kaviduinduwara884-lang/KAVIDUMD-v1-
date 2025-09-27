67ebbfd750f8440e9001824085a946f71d400b4f
kaviduinduwara884-lang <kaviduinduwara884@gmail.com>
2025-09-27 13:35:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'KAVIDU-MD=P9Q2XJ7b#av6ejkWZDACC1_4WtyzLGxbnKCXEpI7mri_I_I5zqg0' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? 'KAVIDU-MD=6IskSZ7S#XLZXjlHCy6B1uBYPBsYjFNYtNQ100vBcN1EMzLcj2a8' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT,    
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'vispermd' : process.env.SESSION_NAME, 
 
